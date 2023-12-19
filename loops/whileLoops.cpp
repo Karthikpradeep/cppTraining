06e989d088201d1890f622419c735e91ac4b3588
Karthik <karthikpradeep64@gmail.com>
2023-12-19 12:02:03 +0530

    first commint

---
+++ loops/whileLoops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int i=5;
+	while(i<5){
+		cout<<"Using while loop"<<endl;
+		cout<<"Hello"<<endl;
+		
+	}
+
+	do{
+		cout<<"Using do while loop"<<endl;
+		cout<<"Hello"<<endl;
+	
+	}while(i<5);
+	return 0;
+}
